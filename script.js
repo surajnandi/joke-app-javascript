1bb16b87954b867bc7266196d821dd9137d2b28a
Suraj Nandi <60923727+surajnandi@users.noreply.github.com>
2021-03-03 08:42:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const JokeBtn = document.getElementById('joke-btn');
+JokeBtn.addEventListener('click', jokeGenHandler);
+
+function jokeGenHandler() {
+    fetch('https://v2.jokeapi.dev/joke/Any')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let joke, jokeDelivery;
+            if (data.joke) {
+                joke = data.joke;
+                jokeDelivery = "";
+            } else {
+                joke = data.setup;
+                jokeDelivery = data.delivery;
+            }
+            document.getElementById('category').innerHTML = data.category;
+            document.getElementById('setup').innerHTML = joke;
+            document.getElementById('delivery').innerHTML = jokeDelivery;
+        });
+}
+jokeGenHandler();
